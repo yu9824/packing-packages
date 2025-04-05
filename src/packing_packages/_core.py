35aacec7ca3cb53a72b76d18c562c8be969f5043
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-04-05 22:01:26 +0900

    feat: Update packing packages
    - Download if not found the conda packages
    - Warn if not match python version
    - Add dry run mode

--- src/packing_packages/_core.py
+++ src/packing_packages/_core.py
@@ -1,84 +1,203 @@
 import os
 import subprocess
 import sys
-import warnings
 from pathlib import Path
 from shutil import copyfile
-from typing import Optional, Union
+from typing import NamedTuple, Optional, Union
 
-DIRPATH_CONDA_ROOT = Path(
-    os.environ.get("CONDA_ROOT", Path(os.environ["CONDA_EXE"]).parent.parent)
-).resolve()
+from packing_packages.logging import get_child_logger
+from packing_packages.utils import is_installed
+
+if is_installed("tqdm"):
+    from tqdm.auto import tqdm
+else:
+    from packing_packages.utils import (  # type: ignore[assignment]
+        dummy_tqdm as tqdm,
+    )
+
+DIRPATH_CONDA_ROOT = Path(os.environ["CONDA_EXE"]).parent.parent.resolve()
 dirpath_pkgs = DIRPATH_CONDA_ROOT / "pkgs"
 
 EXTENSIONS_CONDA = ("tar.bz2", "conda")
 
 
-# TODO: 失敗したパッケージ名を保存しておく
-# TODO: loggingする
-# TODO: tqdmでプログレスバーを出しつつ、stdoutを非表示にする or loggingに流す。
-# TODO: docstring作成
-# pipでダウンロードする場合、同じバージョンのpythonじゃないとダウンロードできないことがある。同じ仮想環境でやるのがよさそう。
+class Package(NamedTuple):
+    name: str
+    version: str
+    build: str
+    channel: str
+
+
+_logger = get_child_logger(__name__)
+
+
 def packing_packages(
     env_name: Optional[str] = None,
-    dirpath_output: Union[os.PathLike, str] = ".",
-):
-    dirpath_output = Path(dirpath_output).resolve()
-    if not dirpath_output.is_dir():
-        raise FileNotFoundError(dirpath_output)
+    dirpath_target: Union[os.PathLike, str] = ".",
+    dry_run: bool = False,
+) -> None:
+    """packaging conda environment packages
+
+    packing_packages
+    ----------
+    env_name : str
+        conda environment name
+    dirpath_target : str
+        target directory path
+    dry_run : bool
+        if True, do not download files
+    """
+    if dry_run:
+        _logger.warning("This is a dry run. No files will be downloaded.")
+
+    if env_name is None:
+        env_name = os.environ["CONDA_DEFAULT_ENV"]
+
+    dirpath_target = Path(dirpath_target).resolve()
+    if not dirpath_target.is_dir():
+        raise FileNotFoundError(dirpath_target)
+
+    dirpath_output = dirpath_target / env_name
+    os.makedirs(dirpath_output, exist_ok=True)
 
     dirpath_output_pypi = dirpath_output / "pypi"
     dirpath_output_conda = dirpath_output / "conda"
     for _dirpath in (dirpath_output_pypi, dirpath_output_conda):
-        os.makedirs(_dirpath, exist_ok=True)
+        if not dry_run:
+            os.makedirs(_dirpath, exist_ok=True)
 
-    command_conda_list = "conda list" + (f" -n {env_name}" if env_name else "")
-    conda_list = (
-        subprocess.run(command_conda_list.split(), stdout=subprocess.PIPE)
-        .stdout.decode("utf-8")
-        .splitlines()
+    result_conda_list = subprocess.run(
+        [
+            os.environ["CONDA_EXE"],
+            "list",
+            "-n",
+            env_name,
+        ],
+        stdout=subprocess.PIPE,
     )
+    conda_list = result_conda_list.stdout.decode("utf-8").splitlines()
 
-    n_failed = 0
-    n_success = 0
+    # conda listの出力をパースする
+    list_packages: list[Package] = []
     for line in conda_list:
         if line[0] == "#":
             continue
         line_split = tuple(line.strip().split())
-        package_name = line_split[0]
-        package_version = line_split[1]
-        package_build = line_split[2]
-        channel = line_split[3] if len(line_split) > 3 else ""
+        package = Package(
+            name=line_split[0],
+            version=line_split[1],
+            build=line_split[2],
+            channel=line_split[3] if len(line_split) > 3 else "",
+        )
+        list_packages.append(package)
+        if package.name == "python":
+            env_python_version = package.version
 
+    # # conda listで取得したpythonのバージョンと、現在のpythonのバージョンが異なる場合は警告を出す
+    if (
+        tuple(map(int, env_python_version.split(".")[:2]))
+        != sys.version_info[:2]
+    ):
+        _logger.warning(
+            "The Python version of the conda environment and the current Python version do not match. "
+            "It is recommended to use the same Python version."
+        )
+
+    list_failed_packages: list[Package] = []
+    for package in tqdm(list_packages):
         # pypiの場合はダウンロードする
-        if channel == "pypi":
-            result_pip_download = subprocess.run(
-                f"{sys.executable} -m pip download {package_name}=={package_version} --no-deps -d {dirpath_output_pypi.as_posix()}".split()
-            )
-            if result_pip_download.returncode == 0:
-                n_success += 1
+        if package.channel == "pypi":
+            if dry_run:
+                result_pip_download = subprocess.run(
+                    [
+                        sys.executable,
+                        "-m",
+                        "pip",
+                        "install",
+                        f"{package.name}=={package.version}",
+                        "--no-deps",
+                        "--python-version",
+                        f"{env_python_version}",
+                        "--dry-run",
+                        "--target",
+                        dirpath_output_pypi.as_posix(),
+                    ],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
             else:
-                warnings.warn(f"{line} is not found.")
-                n_failed += 1
+                result_pip_download = subprocess.run(
+                    [
+                        sys.executable,
+                        "-m",
+                        "pip",
+                        "download",
+                        f"{package.name}=={package.version}",
+                        "--no-deps",
+                        "--python-version",
+                        f"{env_python_version}",
+                        "-d",
+                        dirpath_output_pypi.as_posix(),
+                    ],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
+
+            _logger.info("\n" + result_pip_download.stdout.decode("utf-8"))
+            if result_pip_download.returncode != 0:
+                _logger.warning(f"'{package}' is not found.")
+                _logger.error(result_pip_download.stderr.decode("utf-8"))
+                list_failed_packages.append(package)
 
             continue
 
-        # condaの場合はすでにダウンロードされているやつから探す
-        for ext in EXTENSIONS_CONDA:
-            filename_package = (
-                f"{package_name}-{package_version}-{package_build}.{ext}"
+        tup_filepaths_package = tuple(
+            dirpath_pkgs
+            / f"{package.name}-{package.version}-{package.build}.{ext}"
+            for ext in EXTENSIONS_CONDA
+        )
+        # ないなら、conda installでダウンロードする
+        if not any(
+            filepath_package.is_file()
+            for filepath_package in tup_filepaths_package
+        ):
+            result_conda_download = subprocess.run(
+                [
+                    os.environ["CONDA_EXE"],
+                    "install",
+                    "-y",
+                    "-n",
+                    env_name,
+                    f"{package.name}={package.version}={package.build}",
+                    "--no-deps",
+                    "--download-only",
+                ]
+                + (["-c", package.channel] if package.channel else [])
+                + (["--dry-run"] if dry_run else []),
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
             )
-            filepath_package = dirpath_pkgs / filename_package
-            if filepath_package.is_file():
-                copyfile(
-                    filepath_package, dirpath_output_conda / filename_package
-                )
-                n_success += 1
-                break
+            _logger.info(result_conda_download.stdout.decode("utf-8"))
+            if result_conda_download.returncode != 0:
+                _logger.warning(f"'{package}' is not found.")
+                _logger.error(result_conda_download.stderr.decode("utf-8"))
+                list_failed_packages.append(package)
         else:
-            warnings.warn(f"{line} is not found.")
-            n_failed += 1
-    print(f"{n_success} / {(n_success + n_failed)} packages are success!")
+            for filepath_package in tup_filepaths_package:
+                if filepath_package.is_file():
+                    _logger.info(f"copying {filepath_package.name}")
+                    if not dry_run:
+                        copyfile(
+                            filepath_package,
+                            dirpath_output_conda / filepath_package.name,
+                        )
+                    break
+
+    n_success = len(list_packages) - len(list_failed_packages)
+    n_failed = len(list_failed_packages)
+    _logger.info(
+        f"{n_success} / {(n_success + n_failed)} packages are success!"
+    )
 
 
 if __name__ == "__main__":
