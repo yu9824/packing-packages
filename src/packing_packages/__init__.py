395a62826a0c9af957986840b360a12ed9908abd
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-06-04 22:39:31 +0900

    v0.1.0

--- src/packing_packages/__init__.py
+++ src/packing_packages/__init__.py
@@ -16,7 +16,7 @@ dependencies into a specified directory.
 
 """
 
-__version__ = "0.1.0-rc.0"
+__version__ = "0.1.0"
 __license__ = "MIT"
 __author__ = "yu9824"
 __copyright__ = "Copyright © 2025 yu9824"
