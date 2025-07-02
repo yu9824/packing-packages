6db2f5a3c9d47517fccd6de22212dd4632319b1a
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-07-02 21:44:30 +0900

    docs: Update docstring

--- src/packing_packages/pack/yaml/__init__.py
+++ src/packing_packages/pack/yaml/__init__.py
@@ -1,3 +1,18 @@
+"""Packing packages for distribution and installation.
+
+This module provides functionality to pack conda environments and their
+dependencies into a specified directory.
+
+.. code-block:: bash
+
+    $ python -m packing_packages pack yaml --help
+
+    or
+
+    $ packing-packages pack yaml --help
+
+"""
+
 from ._core import packing_packages_from_yaml
 
 __all__ = ("packing_packages_from_yaml",)
