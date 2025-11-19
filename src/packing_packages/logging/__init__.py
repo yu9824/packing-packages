88f9b7f9222aa9d22085482d1ad6cca9ce9bfb6d
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-11-20 07:15:25 +0900

    refactor: Update logging

--- src/packing_packages/logging/__init__.py
+++ src/packing_packages/logging/__init__.py
@@ -1,27 +1,32 @@
-"""This package provides a set of functions to manage logging in Python applications.
+"""This module provides a set of utilities for logging in Python.
 
-It includes functions to enable or disable the default logging handler, catch the default handler,
-get child loggers, and retrieve the root logger. It also provides constants for different logging levels.
+It includes functions to configure logging behavior, such as enabling or disabling
+the default handler, retrieving a child logger, and accessing the root logger.
+Additionally, this module defines constants for various logging levels to be used
+in logging configurations.
+
 """
 
 from logging import CRITICAL, DEBUG, ERROR, INFO, NOTSET, WARNING
 
 from ._logging import (
+    catch_all_handler,
     catch_default_handler,
     disable_default_handler,
     enable_default_handler,
     get_child_logger,
     get_handler,
-    get_root_logger,
+    get_library_root_logger,
 )
 
 __all__ = (
+    "catch_all_handler",
     "catch_default_handler",
     "disable_default_handler",
     "enable_default_handler",
     "get_child_logger",
     "get_handler",
-    "get_root_logger",
+    "get_library_root_logger",
     "CRITICAL",
     "DEBUG",
     "ERROR",
